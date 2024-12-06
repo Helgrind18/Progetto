b58e5b4be3871707e9a07ec67f9dfa716ad7e78e
Helgrind16 <fiorepasquale2003@gmail.com>
2024-12-06 14:02:36 +0100

    Commento da Ugo

--- app/src/main/java/com/example/progetto/MainActivity.kt
+++ app/src/main/java/com/example/progetto/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         // comm
         // palle pisello
+        //Commento by Ugo
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
