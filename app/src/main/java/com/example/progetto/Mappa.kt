4992aa269593132f1cf9f86e2db25dd693e019f4
Chrustho <christianbruni131@gmail.com>
2024-12-13 11:55:41 +0100

    inizio implementazione sesore posizione

--- app/src/main/java/com/example/progetto/Mappa.kt
+++ app/src/main/java/com/example/progetto/Mappa.kt
@@ -1,13 +1,26 @@
 package com.example.progetto
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class Mappa : AppCompatActivity() {
+    private lateinit var locationManager: LocationManager
+    private val REQUEST_LOCATION_PERMISSION = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_mappa)
@@ -16,5 +29,65 @@ class Mappa : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // Richiedi la permission per accedere alla posizione
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        } else {
+            requestLocationUpdates()
+        }
+    }
+
+    private fun startLocationUpdates() {
+        // Controllo permesso
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            // Richiesta di aggiornamento della posizione
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                5000, // 5 seconds
+                10f,  // 10 meters
+                this
+            ) else {
+            // Informa l'utente dell'assenza di permessi
+            Toast.makeText(this,"Location permission not granted. " +
+                    "Unable to update location.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun requestLocationUpdates() {
+        val MIN_TIME_BW_UPDATES = 5000
+        val MIN_DISTANCE_CHANGE_FOR_UPDATES = 5
+        val locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                val latitude = location.latitude
+                val longitude = location.longitude
+
+                // Salva le coordinate nelle Shared Preferences
+                val sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+                with(sharedPref.edit()) {
+                    putFloat("latitude", latitude.toFloat())
+                    putFloat("longitude", longitude.toFloat())
+                    apply()
+                }
+            }
+        }
+
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            MIN_TIME_BW_UPDATES,
+            MIN_DISTANCE_CHANGE_FOR_UPDATES, this
+        )
     }
 }
