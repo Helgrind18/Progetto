e703aa6b76434c16ffc5847ac98154460d667e63
Helgrind16 <fiorepasquale2003@gmail.com>
2024-12-22 23:02:46 +0100

    Modifica layout biblioteca: TODO: settare meglio la UI, vedere la UI della Home; vedere se crashano i prestiti

--- app/src/main/java/com/example/progetto/Biblioteca.kt
+++ app/src/main/java/com/example/progetto/Biblioteca.kt
@@ -21,7 +21,7 @@ class Biblioteca : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_biblioteca)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gridLayoutBiblioteca)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
