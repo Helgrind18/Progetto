7f5806a8f2178e20972232dfb4fdbc03f5197b85
Chrustho <christianbruni131@gmail.com>
2025-02-01 19:33:41 +0100

    Piano di studio finito, inizio gestione ActivityRiutilizzabile.kt pt.2

--- app/src/main/java/com/example/progetto/dataBase/DataBaseApp.kt
+++ app/src/main/java/com/example/progetto/dataBase/DataBaseApp.kt
@@ -49,7 +49,7 @@ abstract class DataBaseApp : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     DataBaseApp::class.java,
-                    "DatabaseProjectUnical43"
+                    "DatabaseProjectUnical44"
                 )
                     .build()
                 INSTANCE = instance
